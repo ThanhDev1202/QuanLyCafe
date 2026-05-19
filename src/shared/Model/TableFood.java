0dfb0394f21a5099ad39adb26ebec87fa092f35c
haideptrai123-byte <jymmecteam@gmail.com>
2026-05-19 12:13:49 +0700

    Update TableFood.java

--- src/shared/Model/TableFood.java
+++ src/shared/Model/TableFood.java
@@ -4,10 +4,49 @@
  */
 package shared.Model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author admin
  */
-public class TableFood {
+public class TableFood implements Serializable{
+    
+    private int id;
+    private String name;
+    private String status;
+
+    public TableFood() {
+    }
+
+    public TableFood(int id, String name, String status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
     
 }
